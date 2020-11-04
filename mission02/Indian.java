a4204aeb2c2671e40ff802141d02885a50c99228
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-04 19:54:32 +1300

    Thinking of how to make this to methods

--- mission02/Indian.java
+++ mission02/Indian.java
@@ -8,15 +8,51 @@ public class Indian {
 			Scanner s = new Scanner(System.in);
 			System.out.println("생년월일을 차례대로 입력해주세요.");
 			
-			int birth = s.nextInt();
+			int year = s.nextInt();
 			int month = s.nextInt();
 			int day = s.nextInt();
 			
-			if (birth%10 == 0) {
-				System.out.println("시끄러운");
+			String y, m, d;
+			
+			if (year%10 == 0) {
+				y = "시끄러운";
+			}
+		
+			if (year%10 == 1) {
+				y = "푸른";
 			}
 			
+			if (year%10 == 2) {
+				y = "어두운";
+			}
+			
+			if (year%10 == 3) {
+				y = "조용한";
+			}
+			
+			if (year%10 == 4) {
+				y = "웅크린";
+			}
+			
+			if (year%10 == 5) {
+				y = "푸른";
+			}
+			
+			if (year%10 == 6) {
+				y = "지혜로운";
+			}
+			
+			if (year%10 == 7) {
+				y = "용감";
+			}
+			
+			if (year%10 == 8) {
+				y = "날카로운";
+			}
+			
+			if (year%10 == 9) {
+				y = "욕심많은";
+			}
 
 	}
-
 }
