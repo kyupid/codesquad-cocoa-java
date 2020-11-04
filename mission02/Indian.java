d486c0184ec62df7d4f9a95798072fa9ffbae84c
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-04 20:12:09 +1300

    Nana taught me how to make a method

--- mission02/Indian.java
+++ mission02/Indian.java
@@ -3,8 +3,55 @@ package mission02;
 import java.util.Scanner;
 
 public class Indian {
+	
+	public static void indianyear(int year) {
+		String y;
+		
+		if (year%10 == 0) {
+			y = "시끄러운";
+			System.out.println(y);
+			
+		}
+	
+		if (year%10 == 1) {
+			y = "푸른";
+		}
+		
+		if (year%10 == 2) {
+			y = "어두운";
+		}
+		
+		if (year%10 == 3) {
+			y = "조용한";
+		}
+		
+		if (year%10 == 4) {
+			y = "웅크린";
+		}
+		
+		if (year%10 == 5) {
+			y = "푸른";
+		}
+		
+		if (year%10 == 6) {
+			y = "지혜로운";
+		}
+		
+		if (year%10 == 7) {
+			y = "용감";
+		}
+		
+		if (year%10 == 8) {
+			y = "날카로운";
+		}
+		
+		if (year%10 == 9) {
+			y = "욕심많은";
+		}
+	}
 
 	public static void main(String[] args) {
+		
 			Scanner s = new Scanner(System.in);
 			System.out.println("생년월일을 차례대로 입력해주세요.");
 			
@@ -12,47 +59,9 @@ public class Indian {
 			int month = s.nextInt();
 			int day = s.nextInt();
 			
-			String y, m, d;
+			indianyear(year);
 			
-			if (year%10 == 0) {
-				y = "시끄러운";
-			}
-		
-			if (year%10 == 1) {
-				y = "푸른";
-			}
 			
-			if (year%10 == 2) {
-				y = "어두운";
-			}
-			
-			if (year%10 == 3) {
-				y = "조용한";
-			}
-			
-			if (year%10 == 4) {
-				y = "웅크린";
-			}
-			
-			if (year%10 == 5) {
-				y = "푸른";
-			}
-			
-			if (year%10 == 6) {
-				y = "지혜로운";
-			}
-			
-			if (year%10 == 7) {
-				y = "용감";
-			}
-			
-			if (year%10 == 8) {
-				y = "날카로운";
-			}
-			
-			if (year%10 == 9) {
-				y = "욕심많은";
-			}
 
 	}
 }
