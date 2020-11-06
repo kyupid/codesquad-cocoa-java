7b2163e45ae9ab1d6ad1eb0370425c9d1df55641
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-06 22:33:57 +1300

    New P2920
    Re-coded by wrong approach to problem

--- algorithm/P2920.java
+++ algorithm/P2920.java
@@ -1,51 +1,45 @@
 package algorithm;
 
-import java.util.Random;
 import java.util.Scanner;
-
 
 public class P2920 {
 
 	public static void main(String[] args) {
-		
+	
 		Scanner sc = new Scanner(System.in);
-		Random rd = new Random();
 		
-		int[] dajangjo = new int[8];
+		int[] a = new int[8];
 		
-		for ( int i = 0; i < 8; i++ ) {
-			dajangjo[i] = rd.nextInt(8);
-			
-			for ( int j = 0; j < i; j++ ) {
-				if ( dajangjo[j] == dajangjo[i]) {
-					i--;
+		//for문으로 배열에 standard input
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		int[] ascending = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] decending = { 8, 7, 6, 5, 4, 3, 2, 1 };
+		
+		for (int i2 = 0; i2 < a.length; i2++) {
+			if (a[i2] == ascending[i2]) {
+				if (a[7] == ascending[7]) {
+					System.out.println("ascending");
 					break;
 				}
+				continue;
+			}else{
+				System.out.println("mixed");
 			}
-			
 		}
 		
+		for (int i3 = 0; i3 < a.length; i3++) {
+			if (a[i3] == decending[i3]) {
+				if (a[7] == decending[7]) {
+					System.out.println("decending");
+				}
+				continue;
+		}
 		
-		
-	
-		
-		
-		/*
-		 * 숫자 8개를 랜덤으로 입력받아야한다.
-		 * 1. 길이가 8인 배열을 생성한다
-		 * 2. 그 배열 안에 랜덤으로 숫자를 차례대로 집어넣는다. 어떻게 랜덤으로 숫자를 나오게 하지?!! -> 랜덤클래
-		 * 3. 인덱스 0부터 7까지 차례대로 어떤 순서로 나왔는지 판별하고
-		 * 		문제에서 요구하는대로 ascending, descending or mixed를 출력한다. 
-		 */
-		
-		/*
-		 * 랜덤 클래스에서 나온 숫자를 for문에 의해 dajangjo의 0번인덱스부터 7번인덱스까지 차례대로 넣는다.
-		 * 아 근데 숫자는 한번씩만 나와야하는데...어떻게 해결하지? -> 셋클래스, for문
-		 * 마지막 for문으로 겹치는 숫자가 있는지 검사했음 일단 겹치지 않는 숫자 8개가 배열에 담겼
-		 */
-			
-			
+		}
 
 	}
-
 }
---
+++ algorithm/P2920Messedup.java
@@ -0,0 +1,77 @@
+package algorithm;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class P2920Messedup {
+
+	public static void main(String[] args) {
+		
+		
+		Random rd = new Random();
+		
+		int[] dajangjo = new int[8];
+		
+		for ( int i = 0; i < 8; i++ ) {
+			dajangjo[i] = rd.nextInt(8);
+			
+			//Check for duplicate numbers
+			for ( int j = 0; j < i; j++ ) {
+				if ( dajangjo[j] == dajangjo[i]) {
+					i--;
+					break;
+				}
+			}
+	
+		}
+			
+
+		//잘 나오는지 확인
+//		for ( int intname = 0; intname < 8; intname++ ) {
+//			System.out.print(dajangjo[intname]);
+//		}
+		
+		
+		//ascending
+		Loop1 :
+		for ( int k = 0; k < 8; k++ ) {
+			if (k + 1 == dajangjo[k]) {
+				if (dajangjo[7] == 8) {
+					System.out.println("Ascending");
+					break Loop1;
+				}
+				continue;
+			}else{
+				break;
+			}
+		}System.out.println("Mixed");
+		
+		
+		
+		
+	
+	
+	}
+}
+
+		
+		/*
+		 * 숫자 8개를 랜덤으로 입력받아야한다.
+		 * 1. 길이가 8인 배열을 생성한다
+		 * 2. 그 배열 안에 랜덤으로 숫자를 차례대로 집어넣는다. 어떻게 랜덤으로 숫자를 나오게 하지?!! -> 랜덤클래
+		 * 3. 인덱스 0부터 7까지 차례대로 어떤 순서로 나왔는지 판별하고
+		 * 		문제에서 요구하는대로 ascending, descending or mixed를 출력한다. 
+		 */
+		
+		/*
+		 * 랜덤 클래스에서 나온 숫자를 for문에 의해 dajangjo의 0번인덱스부터 7번인덱스까지 차례대로 넣는다.
+		 * 아 근데 숫자는 한번씩만 나와야하는데...어떻게 해결하지? -> 셋클래스, for문
+		 * 마지막 for문으로 겹치는 숫자가 있는지 검사했음 일단 겹치지 않는 숫자 8개가 배열에 담겼
+		 */
+			
+			
+
+	
+
+
