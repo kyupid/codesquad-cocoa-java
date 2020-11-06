de2822796f98cc0f8074f0e3d0765a585350157d
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-06 15:44:08 +1300

    Change datatype
    In integer division, the digits after the decimal point is removed

--- algorithm/P1008.java
+++ algorithm/P1008.java
@@ -7,8 +7,8 @@ public class P1008 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
 		
 		if ( 0 < A && A < 10 && 0 <B && B < 10 ) {
 			System.out.println(A / B);
