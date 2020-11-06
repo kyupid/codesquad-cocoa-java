1fc7f429fa36b8e693b2f86b29da100f7f4aea27
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-06 16:15:19 +1300

    Create P2438.java

---
+++ algorithm/P2438.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class P2438 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String star = "*";
+		
+		for ( int i = 1; i <= N; i++) {
+			
+				System.out.println(star);
+				/*
+				 * 입력한 정수만큼은 별이 나오도록했다. 별이 점점 늘어나려면?
+				 * 일단 문자를 어떻게 이어붙이지?
+				 * 요구사항을 충족하려면 같은 String 타입의 변수를 이어붙여야한다고 생각
+				 */
+				
+		}
+		sc.close();
+	}
+
+}
