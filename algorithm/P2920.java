7f3b9a4073231ab582e5e16fc5d569c4ab36ef47
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-06 19:10:26 +1300

    Create P2920.java

---
+++ algorithm/P2920.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class P2920 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int[] dajangjo = new int[8];
+		
+		
+		/*
+		 * 숫자 8개를 랜덤으로 입력받아야한다.
+		 * 1. 길이가 8인 배열을 생성한다
+		 * 2. 그 배열 안에 랜덤으로 숫자를 차례대로 집어넣는다. 어떻게 랜덤으로 숫자를 나오게 하지?!!
+		 * 3. 인덱스 0부터 7까지 차례대로 어떤 순서로 나왔는지 판별하고
+		 * 		문제에서 요구하는대로 ascending, descending or mixed를 출력한다. 
+		 */
+
+	}
+
+}
