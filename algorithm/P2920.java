4f3d9fcd11e91b4785a8a64ecef46fbacd181448
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-08 11:20:20 +1300

    Corret answer
    but I wanted to use a for loop.

--- algorithm/P2920.java
+++ algorithm/P2920.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2920 {
@@ -10,36 +11,30 @@ public class P2920 {
 		
 		int[] a = new int[8];
 		
-		//for문으로 배열에 standard input
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
 		sc.close();
 		
 		int[] ascending = { 1, 2, 3, 4, 5, 6, 7, 8 };
-		int[] decending = { 8, 7, 6, 5, 4, 3, 2, 1 };
+		int[] descending = { 8, 7, 6, 5, 4, 3, 2, 1 };
 		
-		for (int i2 = 0; i2 < a.length; i2++) {
-			if (a[i2] == ascending[i2]) {
-				if (a[7] == ascending[7]) {
-					System.out.println("ascending");
-					break;
-				}
-				continue;
-			}else{
-				System.out.println("mixed");
-			}
+		//배열이 동일한지 비교하려면 Arrays.equals를 사용한다
+		//모드 배열 형에 대해 사용가능하다.
+		//다차원 배열의 동일성을 확인하려면 Arrays.deepEquals를 사용한다
+		//메소드를 사용하는것 말고 for문을 이용해 확인할 방법은 없나?
+		
+		if (Arrays.equals(a, ascending)) {
+			System.out.println("ascending");
+		}else if(Arrays.equals(a, descending)) {
+			System.out.println("descending");
+		}else {
+			System.out.println("mixed");
 		}
 		
-		for (int i3 = 0; i3 < a.length; i3++) {
-			if (a[i3] == decending[i3]) {
-				if (a[7] == decending[7]) {
-					System.out.println("decending");
-				}
-				continue;
-		}
+		
 		
 		}
 
 	}
-}
+
