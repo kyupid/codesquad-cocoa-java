732e3f29c2e106678cc42899254255a387a14157
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-17 20:59:52 +1300

    Clock output in minutes
    How to delete stuff printed to console by System.out.println()?

--- mission04/Main.java
+++ mission04/Main.java
@@ -12,113 +12,155 @@ public class Main {
 				{ "아", "홉", "열", "한", "두", "시" },
 				{ "자", "이", "삼", "사", "오", "십" },
 				{ "정", "일", "이", "삼", "사", "오" },
-				{ "오", "육", "칠", "팔", "구", "분" } 
+				{ "오", "육", "칠", "팔", "구", "분" },
 				};
 
 		LocalTime cT = LocalTime.now();
 		int h = cT.getHour();
 		int m = cT.getMinute();
+		int s = cT.getSecond();
+		
+		for (int i = 0; i < clock.length; i++) {
+			for (int j = 0; j < clock[0].length; j++) {
+				clock[i][j] = "🤣";
+			}
+		}
+//		for (int i = 0; i < clock.length; i++) {
+//			for (int j = 0; j <clock[0].length; j++) {
+//				System.out.print(clock[i][j]);
+//			}System.out.println();
+//		}		이따가 조건 넣어주고 마지막에 !!
 
-		if (h == 0) {
-			System.out.println(clock[3][0] + clock[4][0]);
+		if (13 <= h && h <= 23) {
+			h = h - 12;
 		}
-		if (h == 1 || h == 13) {
-			System.out.println(clock[0][0] + clock[2][5]);
+		if (h == 0 && m == 0) {
+			clock[3][0] = "자";
+			clock[4][0] = "정";
 		}
-		if (h == 2 || h == 14) {
-			System.out.println(clock[0][1] + clock[2][5]);
+		if (h == 12 && m == 0) {
+			clock[4][0] = "정";
+			clock[5][0] = "오";
 		}
-		if (h == 3 || h == 15) {
-			System.out.println(clock[0][2] + clock[2][5]);
+		if (h == 1) {
+			clock[0][0] = "한";
+			clock[2][5] = "시";
 		}
-		if (h == 4 || h == 16) {
-			System.out.println(clock[0][3] + clock[2][5]);
-		} // 1시에서 4시까지중복
+		if (h == 2) {
+			clock[0][1] = "두";
+			clock[2][5] = "시";
+		}
+		if (h == 3) {
+			clock[0][2] = "세";
+			clock[2][5] = "시";
+		}
+		if (h == 4) {
+			clock[0][3] = "네";
+			clock[2][5] = "시";
+		}
+		if (h == 5) {
+			clock[0][4] = "다";
+			clock[0][5] = "섯";
+			clock[2][5] = "시";
+		}
+		if (h == 6) {
+			clock[1][0] = "여";
+			clock[1][1] = "섯";
+			clock[2][5] = "시";
+		}
+		if (h == 7) {
+			clock[1][2] = "일";
+			clock[1][3] = "곱";
+			clock[2][5] = "시";
+		}
+		if (h == 8) {
+			clock[1][4] = "여";
+			clock[1][5] = "덟";
+			clock[2][5] = "시";
+		}
+		if (h == 9) {
+			clock[2][0] = "아";
+			clock[2][1] = "홉";
+			clock[2][5] = "시";
 
-//		 if (0 < hour && hour < 5) {
-//			 System.out.println(clock[0][hour-1] + clock[2][5]);
-//		 }
-//		 
-//		 if (12 < hour && hour < 17) {
-//			 System.out.println(clock[0][hour-1] + clock[2][5]);
-//		 }
-
-		if (h == 5 || h == 17) {
-			System.out.println(clock[0][4] + clock[0][5] + clock[2][5]);
 		}
-		if (h == 6 || h == 18) {
-			System.out.println(clock[1][0] + clock[1][1] + clock[2][5]);
+		if (h == 10) {
+			clock[2][2] = "열";
+			clock[2][5] = "시";
 		}
-		if (h == 7 || h == 19) {
-			System.out.println(clock[1][2] + clock[1][3] + clock[2][5]);
-		}
-		if (h == 8 || h == 20) {
-			System.out.println(clock[1][4] + clock[1][5] + clock[2][5]);
-		}
-		if (h == 9 || h == 21) {
-			System.out.println(clock[2][0] + clock[2][1] + clock[2][5]);
-		} // 5~9시까지중복
-
-		if (h == 10 || h == 22) {
-			System.out.println(clock[2][2] + clock[2][5]);
-		}
-
-		if (h == 11 || h == 23) {
-			System.out.println(clock[2][2] + clock[2][3] + clock[2][5]);
+		if (h == 11) {
+			clock[2][2] = "열";
+			clock[2][3] = "한";
+			clock[2][5] = "시";
 		}
 		if (h == 12) {
-			System.out.println(clock[2][2] + clock[2][4] + clock[2][5]);
+			clock[2][2] = "열";
+			clock[2][4] = "두";
+			clock[2][5] = "시";
 		}
 
-		
 		if (m / 10 == 1) {
-			System.out.println(clock[3][5]);
+			clock[3][5] = "십";
 		}
 		if (m / 10 == 2) {
-			System.out.println(clock[3][1] + clock[3][5]);
+			clock[3][1] = "이";
+			clock[3][5] = "십";
 		}
 		if (m / 10 == 3) {
-			System.out.println(clock[3][2] + clock[3][5]);
+			clock[3][2] = "삼";
+			clock[3][5] = "십";
 		}
 		if (m / 10 == 4) {
-			System.out.println(clock[3][3] + clock[3][5]);
+			clock[3][3] = "사";
+			clock[3][5] = "십";
 		}
 		if (m / 10 == 5) {
-			System.out.println(clock[3][4] + clock[3][5]);
+			clock[3][4] = "오";
+			clock[3][5] = "십";
 		}
-		
 		if (m % 10 == 1) {
-			System.out.println(clock[4][1]);
+			clock[4][1] = "일";
 		}
 		if (m % 10 == 2) {
-			System.out.println(clock[4][2]);
+			clock[4][2] = "이";
 		}
 		if (m % 10 == 3) {
-			System.out.println(clock[4][3]);
+			clock[4][3] = "삼";
 		}
 		if (m % 10 == 4) {
-			System.out.println(clock[4][4]);
+			clock[4][4] = "사";
 		}
 		if (m % 10 == 5) {
-			System.out.println(clock[4][5]);
+			clock[4][5] = "오";
 		}
 		if (m % 10 == 6) {
-			System.out.println(clock[5][1]);
+			clock[5][1] = "육";
 		}
 		if (m % 10 == 7) {
-			System.out.println(clock[5][2]);
+			clock[5][2] = "칠";
 		}
 		if (m % 10 == 8) {
-			System.out.println(clock[5][3]);
+			clock[5][3] = "팔";
 		}
 		if (m % 10 == 9) {
-			System.out.println(clock[5][4]);
+			clock[5][4] = "구";
 		}
-		
 		if (m != 0) {
-			System.out.println(clock[5][5]);
+			clock[5][5] = "분";
 		}
 		
+		while (true) {
+			for (int i = 0; i < clock.length; i++) {
+				for (int j = 0; j < clock[0].length; j++) {
+					System.out.print(clock[i][j]);
+				}
+				System.out.println();
+			}//for() end
+			try {
+				Thread.sleep(60000);
+			}catch (Exception ex) {
+			}//try catch() end
+		}//while() end
 		
 	}
 }
