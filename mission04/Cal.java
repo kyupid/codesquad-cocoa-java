6f9e5ebdcd1a48d241b1f40c841ae871c0d91198
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-18 16:31:02 +1300

    Update Cal.java

--- mission04/Cal.java
+++ mission04/Cal.java
@@ -15,31 +15,31 @@ public class Cal {
 		this.month = month;
 	}
 	
-	private void getYmd() {
+	private void setYm() {
 		cal.set(Calendar.YEAR, year);
-		cal.set(Calendar.MONTH, month);
+		cal.set(Calendar.MONTH, month - 1);
 	}
+	
+	
 
 	
 	private void init() {
+		
 		cal.set(Calendar.DATE, 1); // Date를 1로 설정
 		int nFirstDayOfMonth = cal.get(Calendar.DAY_OF_WEEK); // 무슨 Day인지 get.
 
-		System.out.println("tes1 : " + nFirstDayOfMonth);
 		System.out.println(month + "월\n"); // 무슨 Month인지 출력
 		System.out.println("일\t" + "월\t" + "화\t" + "수\t" + "목\t" + "금\t" + "토\n");
-		System.out.println("tes2 : " + nFirstDayOfMonth);
 		
 		for (int i = 1; i < nFirstDayOfMonth; i++) { // 시작하는 공백을 채우기위한
-			System.out.println("ㅁ\t");
-			System.out.println("tes3 : " + nFirstDayOfMonth);
+			System.out.print("  \t");
 
 		}
 	}
 	
 	void run() {
+		setYm();
 		init();
-		getYmd();
 		printDates();
 		
 	}
