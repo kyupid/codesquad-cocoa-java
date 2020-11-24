0d91df8289cbbdb8918e6b0385c3f5dd8ebe06bd
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 12:27:07 +1300

    Declare variables

---
+++ example/CardLayoutEventTest.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class CardLayoutEventTest extends Frame {
+	Button first, prev, next, last;
+	Panel buttons;
+	Panel slide;
+	Panel card1, card2, card3, card4, card5; // slide에 포함될 Panel들
+	CardLayout card;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
