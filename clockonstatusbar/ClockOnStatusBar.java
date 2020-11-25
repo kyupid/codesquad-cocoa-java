2a4b56aa06f9d7babe5b3a66a72b8ee6f627f561
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 15:11:26 +1300

    Modify package about time

--- clockonstatusbar/ClockOnStatusBar.java
+++ clockonstatusbar/ClockOnStatusBar.java
@@ -3,21 +3,15 @@ package clockonstatusbar;
 import java.awt.*;
 import java.awt.event.*;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
 
 
 class ClockOnStatusBar {
 
 	public static void main(String[] args) {
 		
-		Date now = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		
-		
-		
-		
-		Frame f = new Frame("Put real-time clock here");
+		Frame f = new Frame();
 	}
 
 }
