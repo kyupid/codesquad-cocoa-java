dc29b12420340793be56eae60fc92a4c9332aeaa
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 14:48:37 +1300

    Make function clock

--- clockonstatusbar/ClockOnStatusBar.java
+++ clockonstatusbar/ClockOnStatusBar.java
@@ -14,13 +14,21 @@ class ClockOnStatusBar extends Frame{
 		setSize(300, 300);
 		setVisible(true);
 		
-		
-		
 	}
+	
+class Clock {
+	String Clock() {
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		String clock = sdf.format(now);
+		return clock;
+	}
+}
 
 	public static void main(String[] args) {
 		
-		ClockOnStatusBar c = new ClockOnStatusBar("ClockOnStatusBar"); 
+		//not working
+		ClockOnStatusBar c = new ClockOnStatusBar(new Clock()); 
 	}
 
 }
