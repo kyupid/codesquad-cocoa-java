a32643afa9de67fcde798675103416670f8ced3a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 16:14:13 +1300

    Finally It works

--- clockonstatusbar/ClockOnStatusBar.java
+++ clockonstatusbar/ClockOnStatusBar.java
@@ -11,27 +11,20 @@ class ClockOnStatusBar {
 
 	public static void main(String[] args) {
 		
-		
 		Frame f = new Frame();
-		f.addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentShown(ComponentEvent e) {
-				Date now = new Date();
-				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-				String clock = sdf.format(now);
-				while (true) {
-					f.setTitle(clock);
-					try {
-						Thread.sleep(1000);
-					}catch (Exception ex) {
-					}
-				}
-			}
-			
-		});
+		
 		f.setVisible(true);
 		
-		
+		while (true) {
+			Date now = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			String clock = sdf.format(now);
+			f.setTitle(clock);
+			try {
+				Thread.sleep(1000);
+			}catch (Exception e) {
+			}
+		}
 		
 		
 		
