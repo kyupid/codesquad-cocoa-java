c0e6db126239379d06a70ee4b582f4691ccabf11
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 15:01:20 +1300

    Create objects for clock

--- clockonstatusbar/ClockOnStatusBar.java
+++ clockonstatusbar/ClockOnStatusBar.java
@@ -3,10 +3,17 @@ package clockonstatusbar;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 class ClockOnStatusBar {
 
 	public static void main(String[] args) {
+		
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		
 		
 		
 		
