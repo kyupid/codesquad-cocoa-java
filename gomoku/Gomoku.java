c50e5787792f47754126238db95b73a1f74e25c5
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-25 20:42:25 +1300

    Write a first logic

---
+++ gomoku/Gomoku.java
@@ -0,0 +1,101 @@
+package gomoku;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+class Gomoku extends Frame implements MouseListener {
+	
+	final int LINE_NUM = 9; //오목판 줄 수
+	final int LINE_WIDTH = 30; //오목판 줄 간
+	final int BOARD_SIZE = LINE_WIDTH * (LINE_NUM-1); // 오목판의 크기
+	final int STONE_SIZE = (int)(LINE_WIDTH * 0.8); // 돌의 크기
+	
+	final int X0; // 오목판 시작위치 x좌표
+	final int Y0; // 오목판 시작위치 y좌
+	
+	final int FRAME_WIDTH; // Frame의 폭
+	final int FRAME_HEIGHT; // Frame의 높
+	
+	Image img = null; 
+	Graphics gImg = null;
+	
+	public Gomoku(String title) {
+		super(title);
+		
+		//Event Handler를 등록한다
+		addMouseListener(this);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing (WindowEvent we) {
+				System.exit(0);
+			}
+		});
+		
+		setVisible(true);
+		
+		// Inset 은 여백을 주는 
+		Insets insets = getInsets(); //화면에 보이기전에는 Insets의 값을 얻을수없다
+		
+		// 오목판 그려질 위치 (LEFT, TOP)의 좌표 X0, Y0를 지정한다.
+		X0 = insets.left + LINE_WIDTH;
+		Y0 = insets.top + LINE_WIDTH;
+		
+		// Frame의 크기를 계산한다.
+		FRAME_WIDTH = BOARD_SIZE + LINE_WIDTH*2 + insets.left + insets.right;
+		FRAME_HEIGHT = BOARD_SIZE + LINE_WIDTH*2 + insets.top + insets.bottom;
+		
+		//Frame을 화면의 (100, 100)의 위치에 계산된 크기로 보이게 한다.
+		setBounds(100, 100, FRAME_WIDTH, FRAME_HEIGHT);
+		
+		img = createImage (FRAME_WIDTH, FRAME_HEIGHT);
+		gImg = img.getGraphics();
+		
+		setResizable(false);
+		drawBoard(gImg);
+	} // Gomoku (String title)
+	
+	public void drawBoard (Graphics g) {
+		for(int i = 0; i<  LINE_NUM; i++) { //대충 오목판그리는 코드
+			g.drawLine(X0, Y0 + i*LINE_WIDTH, X0+BOARD_SIZE, Y0+i*LINE_WIDTH);
+			g.drawLine(X0+i*LINE_WIDTH, Y0, X0+i*LINE_WIDTH, Y0+BOARD_SIZE);
+		}
+	}
+	
+	public void paint (Graphics g) {
+		if(img == null) return;
+		
+		g.drawImage(img, 0, 0, this); //가상화면에그려진 그림을 Frame에 복사
+	}
+	
+	public void mousePressed(MouseEvent e) { //Mouse Listener
+		int x = e.getX(); //마우스 포인터의 x좌표
+		int y = e.getY(); // 마우스 포인터의 y좌표
+		/*
+		(1) . 아래의 로직에 맞게 코드를 작성하시오
+		1. x y . 와 의 값이 오목판의 밖을 벗어난 곳이면 돌을 그리지 않는다
+		2. x y .( ) 와 의 값을 클릭한 곳에서 가장 가까운 교차점으로 변경한다 반올림
+		3. x y (STONE_SIZE) . 와 의 값에서 돌의 크기 의 절반을 빼야 클릭한 곳에 돌이 그려진다
+		4. , (x,y) 눌러진 버튼이 마우스 왼쪽 버튼이면 의 위치에 검은 돌을 그리고
+		눌러진 버튼이 마우스 오른쪽 버튼이면 의 위치에 흰 돌을 그린다 , (x,y) .
+		( .) 흰 돌을 그릴 때 검은 색 테두리도 같이 그린다
+		5. repaint() . 를 호출한다
+		*/
+		
+		// 1. It doesn't draw the stone if x and y cross the line.
+		if (x < FRAME_WIDTH || x > FRAME_WIDTH ) return;
+		if (y < FRAME_HEIGHT || y > FRAME_HEIGHT ) return;
+		
+		
+	}
+	
+	public void mouseClicked(MouseEvent e) {} // MouseListener
+	public void mouseEntered(MouseEvent e) {} // MouseListener
+	public void mouseExited(MouseEvent e) {} // MouseListener
+	public void mouseReleased(MouseEvent e) {} // MouseListener
+	
+
+	public static void main(String[] args) {
+		new Gomoku("Omok Test");
+	}
+
+}
